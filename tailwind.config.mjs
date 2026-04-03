19647be1faedbb80a62245453605a66127b144c2
chriskennel <ckennel@gmail.com>
2026-04-04 00:56:35 +1300

    Initial commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,17 @@
+export default {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'near-black': '#1a1a1a',
+        'off-white': '#f5f4f0',
+        'white': '#ffffff',
+        'forest-green': '#2a6049',
+        'green-tint': '#e8f2ed',
+        'border': '#e0deda',
+        'muted-text': '#6b6b66',
+      },
+    },
+  },
+  plugins: [],
+}
